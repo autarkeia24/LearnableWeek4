632525a14a26b32b87d3118846cb8e4c0014b237
autarkeia24 <114655058+autarkeia24@users.noreply.github.com>
2024-01-29 00:26:54 +0100

    Created the class Movie to be able to make instances of several movies

--- MovieRentalAPI.js
+++ MovieRentalAPI.js
@@ -1,1 +1,30 @@
+class Movie {
+  constructor(title, genre, available) {
+    this.title = title;
+    this.genre = genre;
+    this.available = available;
+  }
+}
 
+class MovieApi {
+  constructor() {
+    this.movies = [];
+  }
+
+  addMovie(title, genre) {
+    const newMovie = new Movie(title, genre, true);
+    this.movies.push(newMovie);
+    console.log(`"${title}" has been added to the library.`);
+  }
+
+  listMovies() {
+    console.log("\n Movies in the library:");
+    this.movies.forEach((movie) => {
+      console.log(
+        `* ${movie.title} (${movie.genre}) - ${
+          movie.available ? "Available" : "Not Available"
+        }`
+      );
+    });
+  }
+  
